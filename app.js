a0ee5d0963d36139a9f7325311eddea4d250b7dd
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-10-09 13:00:31 +0700

    feat: add next public base url

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const router = require("./routes/index")
+require("dotenv").config();
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended : true}))
+const express = require("express");
+const app = express();
+const port = process.env.NEXT_PUBLIC_BASE_URL || 3000;
+const router = require("./routes/index");
 
-app.use('/', router)
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
