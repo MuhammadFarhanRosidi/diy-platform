fb4ca0fe91f3d9a97f245ad5bb0b5847ae53b7a6
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-10-09 12:28:45 +0700

    feat: add cdn flowbite

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,123 +1,158 @@
-const Model = require("../models/model")
+const Model = require("../models/model");
 
 class Controller {
-    static async home(req, res) {
-        try {
-            res.render('index')
-        } catch (error) {
-            res.send(error)
-        }
+  static async home(req, res) {
+    try {
+      res.render("index");
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async authors(req, res) {
-        try {
-            let data = await Model.authors()
-            res.render('authors', {data})
-        } catch (error) {
-            res.send(error)
-        }
+  static async authors(req, res) {
+    try {
+      let data = await Model.authors();
+      res.render("authors", { data });
+    } catch (error) {
+      res.send(error);
     }
-    
-    static async detail(req, res) {
-        try {
-            let data = await Model.detail()
-            res.render('authorDetails', {data})
-        } catch (error) {
-            res.send(error)
-        }
-    }
+  }
 
-    static async posts(req, res) {
-        try {
-            let {search} = req.query
-            let data = await Model.posts(search)
-            res.render('posts', {data})
-        } catch (error) {
-            res.send(error)
-        }
+  static async detail(req, res) {
+    try {
+      let data = await Model.detail();
+      res.render("authorDetails", { data });
+    } catch (error) {
+      res.send(error);
     }
-    
-    static async postDetail(req, res) {
-        try {
-            let {id} = req.params
-            let data = await Model.postDetail(id)
-            res.render('postDetail', {data})
-        } catch (error) {
-            res.send(error)
-        }
-    }
-    
-    static async postAdd(req, res) {
-        try {
-            let {errors} = req.query
-            let data = await Model.authors()
-            res.render('postAdd', {data, errors})
-        } catch (error) {
-            res.send(error)
-        }
-    }
-    
-    static async handlerPostAdd(req, res) {
-        try {
-            let { title, AuthorId, difficulty, estimatedTime, imageUrl, createdDate, description } = req.body
-            await Model.handlerPostAdd(title, AuthorId, difficulty, estimatedTime, imageUrl, createdDate, description)
-            res.redirect('/posts')
-        } catch (error) {
-            if(error.name === "ValidationError") {
-                res.redirect(`/posts/add?errors=${error.errors}`)
-            } else {
-                res.send(error)
-            }
-        }
-    }
+  }
 
-    static async postEdit(req, res) {
-        try {
-            let {errors} = req.query
-            let {id} = req.params
-            let authors = await Model.authors()
-            let data = await Model.postDetail(id)
-            res.render('postEdit', {data, authors, errors})
-        } catch (error) {
-            res.send(error)
-        }
+  static async posts(req, res) {
+    try {
+      let { search } = req.query;
+      let data = await Model.posts(search);
+      res.render("posts", { data });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async handlerPostEdit(req, res) {
-        try {
-            let {id} = req.params
-            let { title, AuthorId, difficulty, estimatedTime, imageUrl, createdDate, description, totalVote } = req.body
-            await Model.handlerPostEdit(title, AuthorId, difficulty, estimatedTime, imageUrl, createdDate, description, totalVote, id)
-            res.redirect('/posts')
-        } catch (error) {
-            let {id} = req.params
-            if(error.name === 'ValidationError') {
-                res.redirect(`/posts/${id}/edit?errors=${error.errors}`)
-            } else {
-                res.send(error)
-            }
-        }
+  static async postDetail(req, res) {
+    try {
+      let { id } = req.params;
+      let data = await Model.postDetail(id);
+      res.render("postDetail", { data });
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    static async postDelete(req, res) {
-        try {
-            let {id} = req.params
-            await Model.postDelete(id)
-            res.redirect('/posts')
-        } catch (error) {
-            res.send(error)
-        }
+  static async postAdd(req, res) {
+    try {
+      let { errors } = req.query;
+      let data = await Model.authors();
+      res.render("postAdd", { data, errors });
+    } catch (error) {
+      res.send(error);
     }
-    
-    static async vote(req, res) {
-        try {
-            let {id} = req.params
-            await Model.vote(id)
-            res.redirect(`/posts/${id}`)
-        } catch (error) {
-            res.send(error)
-        }
+  }
+
+  static async handlerPostAdd(req, res) {
+    try {
+      let {
+        title,
+        AuthorId,
+        difficulty,
+        estimatedTime,
+        imageUrl,
+        createdDate,
+        description,
+      } = req.body;
+      await Model.handlerPostAdd(
+        title,
+        AuthorId,
+        difficulty,
+        estimatedTime,
+        imageUrl,
+        createdDate,
+        description
+      );
+      res.redirect("/posts");
+    } catch (error) {
+      if (error.name === "ValidationError") {
+        res.redirect(`/posts/add?errors=${error.errors}`);
+      } else {
+        res.send(error);
+      }
     }
+  }
+
+  static async postEdit(req, res) {
+    try {
+      let { errors } = req.query;
+      let { id } = req.params;
+      let authors = await Model.authors();
+      let data = await Model.postDetail(id);
+      res.render("postEdit", { data, authors, errors });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async handlerPostEdit(req, res) {
+    try {
+      let { id } = req.params;
+      let {
+        title,
+        AuthorId,
+        difficulty,
+        estimatedTime,
+        imageUrl,
+        createdDate,
+        description,
+        totalVote,
+      } = req.body;
+      await Model.handlerPostEdit(
+        title,
+        AuthorId,
+        difficulty,
+        estimatedTime,
+        imageUrl,
+        createdDate,
+        description,
+        totalVote,
+        id
+      );
+      res.redirect("/posts");
+    } catch (error) {
+      let { id } = req.params;
+      if (error.name === "ValidationError") {
+        res.redirect(`/posts/${id}/edit?errors=${error.errors}`);
+      } else {
+        res.send(error);
+      }
+    }
+  }
+
+  static async postDelete(req, res) {
+    try {
+      let { id } = req.params;
+      await Model.postDelete(id);
+      res.redirect("/posts");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  static async vote(req, res) {
+    try {
+      let { id } = req.params;
+      await Model.vote(id);
+      res.redirect(`/posts/${id}`);
+    } catch (error) {
+      res.send(error);
+    }
+  }
 }
 
-module.exports = Controller
+module.exports = Controller;
--- setup/seed.js
+++ setup/seed.js
@@ -1,28 +1,30 @@
-const pool = require("./connection")
+const pool = require("./connection");
 const fs = require("fs").promises;
 
 (async () => {
-    try {
-        let author = JSON.parse(await fs.readFile("./data/authors.json", "utf8"))
-        .map(el => {
-            return `('${el.fullName}', '${el.gender}')`
-        })
-        let post = JSON.parse(await fs.readFile("./data/posts.json", "utf8"))
-        .map(el => {
-            return `('${el.title}', '${el.difficulty}', ${el.estimatedTime}, '${el.description}', ${el.totalVote}, '${el.imageUrl}', '${el.createdDate}', ${el.AuthorId})`
-        })
-        let insertAuthors = `
+  try {
+    let author = JSON.parse(
+      await fs.readFile("./data/authors.json", "utf8")
+    ).map((el) => {
+      return `('${el.fullName}', '${el.gender}')`;
+    });
+    let post = JSON.parse(await fs.readFile("./data/posts.json", "utf8")).map(
+      (el) => {
+        return `('${el.title}', '${el.difficulty}', ${el.estimatedTime}, '${el.description}', ${el.totalVote}, '${el.imageUrl}', '${el.createdDate}', ${el.AuthorId})`;
+      }
+    );
+    let insertAuthors = `
         INSERT INTO "Authors" ("fullName", gender)
         VALUES ${author}
-        `
-        let insertPosts = `
+        `;
+    let insertPosts = `
         INSERT INTO "Posts" (title, difficulty, "estimatedTime", description, "totalVote", "imageUrl", "createdDate", "AuthorId")
         VALUES ${post}
-        `
-        await pool.query(insertAuthors)
-        await pool.query(insertPosts)
-        console.log("SUCCESS SEEDING")
-    } catch (error) {
-        console.log("ERROR SEEDING", error)
-    }
-})()
+        `;
+    await pool.query(insertAuthors);
+    await pool.query(insertPosts);
+    console.log("SUCCESS SEEDING");
+  } catch (error) {
+    console.log("ERROR SEEDING", error);
+  }
+})();
